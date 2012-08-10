0c3d4798343150cb5eae177f2e76b3b7c04f0002
Seo Cahill <seo.cahill@gmail.com>
2012-08-10 12:11:26 -0700

    added users and events links to nav, tests passing

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -8,8 +8,15 @@ describe Admin::UsersController do
       sign_in(:user, user)
     end
     
-    it "are not able to access the index action" do
+    it "are able to access the index action" do
       get 'index'
+      response.should render_template("index")
+     # flash[:alert].should eql("You must be an admin to do that.")
+    end
+
+
+    it "are not able to access the new action" do
+      get 'new'
       response.should redirect_to(root_path)
       flash[:alert].should eql("You must be an admin to do that.")
     end
