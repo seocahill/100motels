f44f132f9911487367ebdb12b5eb60875d240c75
Seo Cahill <seo.cahill@gmail.com>
2012-08-13 12:33:46 -0700

    shoulda test for dep des has many between cart/line_itmes passing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120813191857) do
+ActiveRecord::Schema.define(:version => 20120813192848) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(:version => 20120813191857) do
     t.datetime "updated_at",                                 :null => false
     t.decimal  "ticket_price", :precision => 8, :scale => 2
   end
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "event_id"
+    t.integer  "cart_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "line_items", ["cart_id"], :name => "index_line_items_on_cart_id"
+  add_index "line_items", ["event_id"], :name => "index_line_items_on_event_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
