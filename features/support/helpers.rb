b09b7f29e8b0d89729d6e9864cc3727920bd317b
Seo Cahill <seo.cahill@gmail.com>
2012-08-16 22:37:59 -0700

    added launchy support method

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -1,3 +1,11 @@
+#Env variable for passing integers to stepdefs
+
 CAPTURE_A_NUMBER = Transform /^\d+$/ do |number|
 number.to_i
+end
+
+# Call with LAUNCHY=true cucumber
+
+After do |scenario|
+  save_and_open_page if scenario.status == :failed &&  ENV['LAUNCHY'].present?
 end
