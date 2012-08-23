15729d999ce196e69cb8bbf860336c1783d488e1
Seo Cahill <seo.cahill@gmail.com>
2012-08-23 16:45:28 -0700

    users index marked up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_filter :find_user, :only => [:show, :edit, :update, :destroy]
 
   def index
+    @cart = current_cart
     @users = User.all(order: "email")
   end
   
