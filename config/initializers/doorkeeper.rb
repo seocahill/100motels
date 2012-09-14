1e12f14b5bbde1016159c6460b83763633a994a4
Seo Cahill <seo.cahill@gmail.com>
2012-09-14 02:03:16 -0700

    added admin only authentication to doorkeeper app creator

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -14,13 +14,13 @@ Doorkeeper.configure do
   # If you want to restrict the access to the web interface for
   # adding oauth authorized applications you need to declare the
   # block below
-  # admin_authenticator do |routes|
+  admin_authenticator do |routes|
   #   # Put your admin authentication logic here.
   #   # If you want to use named routes from your app you need
   #   # to call them on routes object eg.
   #   # routes.new_admin_session_path
-  #   Admin.find_by_id(session[:admin_id]) || redirect_to(routes.new_admin_session_url)
-  # end
+    current_user.try(:admin?) || redirect_to(routes.root_path)
+  end
 
   # Access token expiration time (default 2 hours).
   # If you want to disable expiration, set this to nil.
