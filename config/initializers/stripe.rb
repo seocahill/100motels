dce56f4ab0d10d5a1a81a2ea77e32e1af7ed2a52
Seo Cahill <seo.cahill@gmail.com>
2012-09-25 18:09:55 -0700

    adding stripe auth

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 if Rails.env == "production"
   # set credentials from ENV hash
-  stripe = {"app_id" => ENV['STRIPE_PUBLIC_KEY'], "secret" => ENV['STRIPE_API_KEY']}
+  stripe = {"app_id" => ENV['STRIPE_PUBLIC_KEY'], "secret" => ENV['STRIPE_API_KEY'], "client" => ENV['STRIPE_PLATFORM_CLIENT_ID']}
 else
   # get credentials from YML file
   stripe = YAML.load(File.open(Rails.root.join("config/stripe.yml")))
@@ -8,3 +8,5 @@ end
 
 Stripe.api_key = stripe["secret"]
 STRIPE_PUBLIC_KEY = stripe["app_id"]
+ENV['STRIPE_PLATFORM_CLIENT_ID'] = "ca_0Qb67hX8UIxDZqR1h7De7OPt0UMe0cRH"
+ENV['STRIPE_API_KEY'] = "YPB5mXrlxEByHnGwBN7c34gKFXOwVWhb"
