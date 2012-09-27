ba62f29904bb0db92cb10089372fbd9784cc8a3d
Seo Cahill <seo.cahill@gmail.com>
2012-09-26 20:21:50 -0700

    changed to foreman for env varibales in dev env

--- config/initializers/gmail.rb
+++
@@ -1,4 +0,0 @@
-if Rails.env == "development"
-  gmail = YAML.load_file("#{::Rails.root}/config/gmail.yml")[::Rails.env]
-  GMAIL_PASSWORD = gmail["password"]
-end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,12 +1,2 @@
-if Rails.env == "production"
-  # set credentials from ENV hash
-  stripe = {"app_id" => ENV['STRIPE_PUBLIC_KEY'], "secret" => ENV['STRIPE_API_KEY'], "client" => ENV['STRIPE_PLATFORM_CLIENT_ID']}
-else
-  # get credentials from YML file
-  stripe = YAML.load(File.open(Rails.root.join("config/stripe.yml")))
-end
-
-Stripe.api_key = stripe["secret"]
-STRIPE_PUBLIC_KEY = stripe["app_id"]
-ENV['STRIPE_PLATFORM_CLIENT_ID'] = "ca_0Qb67hX8UIxDZqR1h7De7OPt0UMe0cRH"
-ENV['STRIPE_API_KEY'] = "YPB5mXrlxEByHnGwBN7c34gKFXOwVWhb"
+Stripe.api_key = ENV['STRIPE_API_KEY']
+STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY']
