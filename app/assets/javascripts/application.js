bb33a5b7ca951d41c3856ba2d7436ced3620f783
Seo Cahill <seo.cahill@gmail.com>
2012-10-10 10:09:08 -0700

    going to bypass the cart so started a new branch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require bootstrap-dropdown.js
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require pages
 //= require jquery.scrollTo
 
 
