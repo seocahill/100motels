8a7246945e4223bc01dbdd11ac98f1ced968f792
Seo Cahill <seo.cahill@gmail.com>
2012-10-12 22:40:36 -0700

    geolocating working on join

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,9 @@ class PagesController < ApplicationController
 
 
   def home
-    @cart = current_cart
-    @events = Event.all(limit: 3)
+    locations = Location.near("Vancouver, Canada", 3000, order: :distance)
+    @events = locations.map{ |l| l.event }.uniq unless locations.nil?
+    # @events = Event.all
     @users = User.all(order: :email, limit: 4)
   end
 
