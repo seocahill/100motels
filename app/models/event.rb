32f49b88a237724519094accbf3ec3901090a230
Seo Cahill <seo.cahill@gmail.com>
2012-10-12 19:13:23 -0700

    setting up relation between event and location

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,7 @@ class Event < ActiveRecord::Base
                   :music, :video, :about, :image, :target
   validates :artist, :venue, :date, :ticket_price, presence: true
 
+  has_one  :location
   has_many :line_items, dependent: :destroy
   has_many :orders, through: :line_items
   has_and_belongs_to_many :users
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,7 @@ class Location < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
+
+  belongs_to :event
 end
 
