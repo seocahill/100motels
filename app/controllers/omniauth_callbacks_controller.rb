1ff24c7a09dc3c446b86cd9c99195d34634b3ec1
Seo Cahill <seo.cahill@gmail.com>
2012-10-15 21:32:44 -0700

    adding separate auth strategy

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -9,5 +9,5 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
-  alias_method :stripe_platform, :all
+  alias_method :stripe_connect, :all
 end
