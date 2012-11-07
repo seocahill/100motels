6af36937a9f0e6411173d21663680b74ac61f12c
Seo Cahill <seo.cahill@gmail.com>
2012-11-06 19:52:03 -0800

    mistake

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -16,6 +16,7 @@ class Profile < ActiveRecord::Base
 
   auto_html_for :promoter_media do
     html_escape
+    image
     flickr
     youtube(:width => 630, :height => 430)
     vimeo(:width => 630, :height => 430)
