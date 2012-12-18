4cdec5b7f33421d6bf6f25d66225b447dd31f213
Seo Cahill <seo.cahill@gmail.com>
2012-12-18 12:58:05 +0000

    adding links to all images homepage location hack

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @location ||= calculate_location
-    @locations = Location.joins(:event).where("events.state > 0 and events.state < 3").uniq.limit(6)
+    # @locations = Location.joins(:event).where("events.state > 0 and events.state < 3").uniq.limit(6)
     @events = Event.where("events.state > 0 and events.state < 3").uniq.limit(6)
     @profiles = Profile.where("state > ? and visible = ?", 0, true).limit(8)
   end
