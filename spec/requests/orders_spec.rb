d098cf74ea85d9bda14619f376651cbcc6aa21c6
Seo Cahill <seo.cahill@gmail.com>
2013-01-09 00:27:34 +0000

    removed binstubs directory as no longer using rvm

---
+++ spec/requests/orders_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "StripeButtonOrder" do
+
+  let (:user) { FactoryGirl.create(:user) }
+  let (:event) { FactoryGirl.create(:event) }
+
+  it "show Success Message after purchase" do
+    pending
+  end
+end
