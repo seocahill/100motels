63430b716453793dbb7151ab54128c0c62417a60
Seo Cahill <seo.cahill@gmail.com>
2013-01-10 21:00:38 +0000

    setup case statement to handle stripe events

--- app/controllers/stripe_events_controller.rb
+++ app/controllers/stripe_events_controller.rb
@@ -1,14 +1,13 @@
 class StripeEventsController < ApplicationController
-  require 'json'
 
   def listen
     data = JSON.parse(request.body.read)
-    print logger.info "Received event with ID: #{data["id"]} Type: #{data["type"]}"
-    render text: '', head: :ok
-
     Stripe.api_key = ENV['STRIPE_API_KEY']
     event = Stripe::Event.retrieve(data["id"])
-
-    StripeEventsHandler.new(event).process_stripe_event
+    if StripeEventsHandler.new(event).process_stripe_event
+      render text: '', head: :ok
+    else
+      render text: '', head: :bad_request
+    end
   end
 end
--- app/services/stripe_events_handler.rb
+++ app/services/stripe_events_handler.rb
@@ -5,8 +5,15 @@ class StripeEventsHandler
   end
 
   def process_stripe_event
-   if @stripe_event.type == 'charge.succeeded'
-      print Rails.logger.debug 'webhooks working!'
+    case @stripe_event.type
+    when 'charge.succeeded' then charge_succeeded
+    when 'charge.failed' then charge_failed
+    else false
     end
+  end
+
+
+  def charge_succeeded
+    print Rails.logger.debug 'webhooks working!'
   end
 end
