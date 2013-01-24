607a843e4aacdb54f17f5ed15c2000b32f367d26
Seo Cahill <seo.cahill@gmail.com>
2013-01-24 20:14:49 +0000

    BIP working with decorators now but how to do wen getter and setting in location model

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -2,20 +2,22 @@ class EventDecorator < ApplicationDecorator
   delegate_all
 
   def header
-    h.content_tag :header, h.title(model.artist, model.venue, model.date)
+    content_tag :header, h.title(model.artist, model.venue, model.date)
   end
 
   def visible
-    h.best_in_place_if h.current_user, model, :visible, :type => :checkbox
+    best_in_place_if event_owner?, model, :visible, :type => :checkbox
   end
 
   def edit
-    h.link_to "Edit in form", h.edit_organizer_event_path(model), class: "btn" if h.current_user
+    link_to "Edit in form", edit_organizer_event_path(model), class: "btn" if event_owner?
   end
 
   def filepicker
-    form_for model do |f|
-      f.filepicker_field(:image, onchange: "this.form.submit();")
+    if event_owner?
+      form_for model do |f|
+        f.filepicker_field(:image, onchange: "this.form.submit();")
+      end
     end
   end
 
@@ -28,19 +30,19 @@ class EventDecorator < ApplicationDecorator
   end
 
   def title
-    best_in_place_if current_user, model, :artist
+    best_in_place_if event_owner?, model, :artist
   end
 
   def date
-    best_in_place_if current_user, model, :date, type: :date, display_with: lambda { |v| v.strftime("%a, %d %b %Y") }
+    best_in_place_if event_owner?, model, :date, type: :date, display_with: lambda { |v| v.strftime("%a, %d %b %Y") }
   end
 
   def venue
-    best_in_place model, :venue
+    best_in_place_if event_owner?, model, :venue
   end
 
   def doors
-    best_in_place model, :doors, display_with: lambda { |v| v.strftime("%l:%M %p")}
+    best_in_place_if event_owner?, model, :doors, display_with: lambda { |v| v.strftime("%l:%M %p")}
   end
 
   def sales
@@ -48,20 +50,20 @@ class EventDecorator < ApplicationDecorator
   end
 
   def price
-    best_in_place_if current_user, model, :ticket_price, display_with: :number_to_currency
+    best_in_place_if event_owner?, model, :ticket_price, display_with: :number_to_currency
   end
 
   def about
-    best_in_place model, :about, :type => :textarea, :ok_button => 'Save', :cancel_button => 'Cancel',
+    best_in_place_if event_owner?, model, :about, :type => :textarea, :ok_button => 'Save', :cancel_button => 'Cancel',
                           :nil => "Click me to add content!", :display_with => :simple_format
   end
 
   def location
-    model.location.address
+    best_in_place_if event_owner?, model, :new_location
   end
 
   def video
-    best_in_place_if current_user, model, :video, :nil => "Click me to add a Youtube or Vimeo url, e.g. http://youtu.be/TZLwfyVYJJw"
+    best_in_place_if event_owner?, model, :video, :nil => "Click me to add a Youtube or Vimeo url, e.g. http://youtu.be/TZLwfyVYJJw"
   end
 
   def video_iframe
@@ -73,7 +75,7 @@ class EventDecorator < ApplicationDecorator
   end
 
   def music
-    best_in_place_if current_user, model, :music, :nil => "Click me to add a Soundcloud url, e.g. http://youtu.be/TZLwfyVYJJw"
+    best_in_place_if event_owner?, model, :music, :nil => "Click me to add a Soundcloud url, e.g. http://youtu.be/TZLwfyVYJJw"
   end
 
   def music_iframe
@@ -82,5 +84,10 @@ class EventDecorator < ApplicationDecorator
     else
       image_tag "//quickimage.it/565x170&text=Soundcloud+playlist", class: "align_left"
     end
+  end
+
+private
+  def event_owner?
+    current_user.id == model.users.first.id
   end
 end
