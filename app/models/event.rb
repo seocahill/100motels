477494f0c45adb3ea681a11d885468a1216221f3
Seo Cahill <seo.cahill@gmail.com>
2013-01-27 17:46:36 +0000

    switched off auto html for now

--- app/models/event.rb
+++ app/models/event.rb
@@ -32,19 +32,19 @@ class Event < ActiveRecord::Base
     end
   end
 
-  auto_html_for :video do
-    html_escape
-    youtube(:width => 630, :height => 430)
-    vimeo(:width => 630, :height => 430)
-    link :target => "_blank", :rel => "nofollow"
-    simple_format
-  end
+  # auto_html_for :video do
+  #   html_escape
+  #   youtube(:width => 630, :height => 430)
+  #   vimeo(:width => 630, :height => 430)
+  #   link :target => "_blank", :rel => "nofollow"
+  #   simple_format
+  # end
 
-  auto_html_for :music do
-    html_escape
-    soundcloud(:width => 630, :height => 200)
-    link :target => "_blank", :rel => "nofollow"
-    simple_format
-  end
+  # auto_html_for :music do
+  #   html_escape
+  #   soundcloud(:width => 630, :height => 200)
+  #   link :target => "_blank", :rel => "nofollow"
+  #   simple_format
+  # end
 end
 
