f91728e3dfceaa9adca5d2594ea1184eb0b58d1a
Seo Cahill <seo.cahill@gmail.com>
2013-02-27 23:47:23 +0000

    hold off fontface declaration yet to see if https works

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module OneHundredMotels
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/app/models/concerns)
+    # config.autoload_paths += %W(#{config.root}/app/models/concerns)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -58,6 +58,8 @@ module OneHundredMotels
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Heroku requires this to be false
     config.assets.initialize_on_precompile = false
