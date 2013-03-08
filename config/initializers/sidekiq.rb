9f16d53ff926cac27c734f7562b465b6b6892859
Seo Cahill <seo.cahill@gmail.com>
2013-03-08 21:09:27 +0000

    sidekiq setup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,15 +2,11 @@ require 'sidekiq'
 require 'sidekiq/web'
 
 Sidekiq.configure_client do |config|
-  # config.redis = { :size => 1 }
+  config.redis = { url: ENV["REDIS_ADDRESS"], namespace: "my-workers" }
 end
 
 Sidekiq.configure_server do |config|
-  # The config.redis is calculated by the
-  # concurrency value so you do not need to
-  # specify this. For this demo I do
-  # show it to understand the numbers
-  # config.redis = { :size => 9 }
+  config.redis = { url: ENV["REDIS_ADDRESS"], namespace: "my-workers" }
 end
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
