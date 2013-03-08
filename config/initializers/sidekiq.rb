1b2d2e9585483eb649bd9e1a307ab3de76f43f01
Seo Cahill <seo.cahill@gmail.com>
2013-03-08 20:20:01 +0000

    delted sidekiq yaml

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@ require 'sidekiq'
 require 'sidekiq/web'
 
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
+  # config.redis = { :size => 1 }
 end
 
 Sidekiq.configure_server do |config|
@@ -10,7 +10,7 @@ Sidekiq.configure_server do |config|
   # concurrency value so you do not need to
   # specify this. For this demo I do
   # show it to understand the numbers
-  config.redis = { :size => 9 }
+  # config.redis = { :size => 9 }
 end
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
