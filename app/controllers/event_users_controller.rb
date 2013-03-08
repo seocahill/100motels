af36db9f8ceff92d07a8d14081eb701a8cc4cbac
Seo Cahill <seo.cahill@gmail.com>
2013-03-08 18:41:02 +0000

    adding back sidekiq worker

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -1,8 +1,7 @@
 class EventUsersController < ApplicationController
-  # GET /event_users
-  # GET /event_users.json
   def index
-    @event_users = EventUser.all
+    @event = Event.find(params[:event_id])
+    @event_users = @event.event_users.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,6 +12,7 @@ class EventUsersController < ApplicationController
   # GET /event_users/1
   # GET /event_users/1.json
   def show
+    @event = Event.find(params[:event_id])
     @event_user = EventUser.find(params[:id])
 
     respond_to do |format|
@@ -24,31 +24,40 @@ class EventUsersController < ApplicationController
   # GET /event_users/new
   # GET /event_users/new.json
   def new
+    @event = Event.find(params[:event_id])
     @event_user = EventUser.new
 
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @event_user }
+      format.js
     end
   end
 
   # GET /event_users/1/edit
   def edit
+    @event = Event.find(params[:event_id])
     @event_user = EventUser.find(params[:id])
   end
 
   # POST /event_users
   # POST /event_users.json
   def create
-    @event_user = EventUser.new(params[:event_user])
-
+    @event = Event.find(params[:event_id])
+    profile = MemberProfile.find_by_email(params[:email])
+    if profile.present?
+      @event_user = @event.event_users.new(user_id: profile.user.id, state: params[:state])
+    else
+    end
     respond_to do |format|
       if @event_user.save
         format.html { redirect_to @event_user, notice: 'Event user was successfully created.' }
         format.json { render json: @event_user, status: :created, location: @event_user }
+        format.js
       else
         format.html { render action: "new" }
         format.json { render json: @event_user.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
@@ -56,6 +65,7 @@ class EventUsersController < ApplicationController
   # PUT /event_users/1
   # PUT /event_users/1.json
   def update
+    @event = Event.find(params[:event_id])
     @event_user = EventUser.find(params[:id])
 
     respond_to do |format|
@@ -72,6 +82,7 @@ class EventUsersController < ApplicationController
   # DELETE /event_users/1
   # DELETE /event_users/1.json
   def destroy
+    @event = Event.find(params[:event_id])
     @event_user = EventUser.find(params[:id])
     @event_user.destroy
 
--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -1,6 +1,7 @@
 class EventUser < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
-  attr_accessible :user_id, :state
+  attr_accessible :user_id, :state, :event_id
+  attr_accessor :email
   enum_accessor :state, [ :reader, :editor, :event_admin, :organizer ]
 end
