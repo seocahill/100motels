7d19f8d68a649961ab166fa3ca71b982861778dc
SeoCahill <seo.cahill@gmail.com>
2013-03-19 23:40:31 +0000

    changed email protocal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ OneHundredMotels::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Set the mailer to localhost
-  config.action_mailer.default_url_options = { :host => '100motels.com' }
+  config.action_mailer.default_url_options = { :host => 'www.100motels.com', :protocol => 'https' }
 
   config.action_mailer.delivery_method = :smtp
 
