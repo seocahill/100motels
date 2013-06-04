c731f9676daf24bab328e2452bb614b40f77d2fb
Seo Cahill <seo.cahill@gmail.com>
2013-06-04 19:30:31 -0400

    view events feature passing

---
+++ spec/features/view_events_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+feature 'View Events' do
+
+  background do
+    @event = create :event, :visible, date: (3).months.from_now
+    visit events_path
+  end
+
+  scenario 'list published Events only' do
+    hidden = create :event
+    expect(page).to have_content(@event.artist)
+    expect(page).to have_content(@event.title)
+    expect(page).to have_content(@event.date.strftime('%d/%m/%Y'))
+    expect(page).to have_content(@event.location.city)
+    expect(page).to_not have_content(hidden.artist)
+  end
+
+  scenario 'Search for an Event' do
+    new_event = create :event, :visible
+    fill_in 'query', with: new_event.venue
+    click_button 'Go'
+    expect(current_path).to eq events_path
+    expect(page).to have_content new_event.artist
+    expect(page).to_not have_content @event.venue
+  end
+
+  scenario 'Filter Events on tonight' do
+    tonight = create :event, :visible, date: (1).hour.from_now
+    click_link 'Filter by Date'
+    click_link 'Today'
+    expect(page).to have_content(tonight.artist)
+    expect(page).to_not have_content(@event.artist)
+  end
+
+  scenario 'Filter Events on this week' do
+    this_week = create :event, :visible, date: (2).days.from_now
+    click_link 'Filter by Date'
+    click_link 'This Week'
+    expect(page).to have_content(this_week.artist)
+    expect(page).to_not have_content(@event.artist)
+  end
+
+  scenario 'Filter Events on this month' do
+    this_month = create :event, :visible, date: (2).weeks.from_now
+    click_link 'Filter by Date'
+    click_link 'This Month'
+    expect(page).to have_content(this_month.artist)
+    expect(page).to_not have_content(@event.artist)
+  end
+
+  scenario 'Browse to individual event page' do
+    first('.thumbnail').click
+    expect(current_path).to eq event_path(@event)
+  end
+end
