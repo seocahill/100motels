c325bb8c919c5eaa8056f209d8dad9066685732d
Seo Cahill <seo.cahill@gmail.com>
2013-06-25 00:50:19 -0400

    old stuff gone

--- app/models/private_message.rb
+++
@@ -1,9 +0,0 @@
-class PrivateMessage
-  include ActiveAttr::Model
-
-  attr_accessor :subject, :email, :content, :organizer_email
-
-  validates :subject, presence: true
-  validates :email, presence: true
-  validates :content, presence: true, length: { maximum: 300 }
-end
