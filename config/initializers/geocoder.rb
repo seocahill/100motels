cecc240475ca295396f82d86a09af7b7304c3125
Seo Cahill <seo.cahill@gmail.com>
2013-06-28 18:05:41 -0400

    ranelag

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,19 +1,19 @@
-Geocoder.configure(
-  :lookup => :test,
-  :cache => Redis.new
-  )
+# Geocoder.configure(
+#   :lookup => :test,
+#   :cache => Redis.new
+#   )
 
-Geocoder::Lookup::Test.set_default_stub(
-  [
-    {
-      'latitude'     => 40.7143528,
-      'longitude'    => -74.0059731,
-      'address'      => 'New York, NY, USA',
-      'city'         => 'New York',
-      'state'        => 'New York',
-      'state_code'   => 'NY',
-      'country'      => 'United States',
-      'country_code' => 'US'
-    }
-  ]
-)
+# Geocoder::Lookup::Test.set_default_stub(
+#   [
+#     {
+#       'latitude'     => 40.7143528,
+#       'longitude'    => -74.0059731,
+#       'address'      => 'New York, NY, USA',
+#       'city'         => 'Preston',
+#       'state'        => 'New York',
+#       'state_code'   => 'NY',
+#       'country'      => 'United States',
+#       'country_code' => 'US'
+#     }
+#   ]
+# )
