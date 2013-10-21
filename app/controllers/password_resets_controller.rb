c7a67f355abf525cce82057cf499f56b9516bc87
Seo Cahill <seo.cahill@gmail.com>
2013-10-21 15:56:36 +0200

    password resets working now

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -19,11 +19,17 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(password_reset_token: params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset has expired."
-    elsif @user.update_attributes(params[:user])
-      @user.state = :normal if @user.user.state_unconfirmed?
-      redirect_to signin_path, :notice => "Password has been reset."
+    elsif @user.update_attributes(reset_password_params)
+      @user.state = :normal if @user.state_unconfirmed?
+      redirect_to login_path, :notice => "Password has been reset."
     else
       render :edit
     end
   end
+
+  private
+
+    def reset_password_params
+      params.require(:user).permit(:state, :password)
+    end
 end
