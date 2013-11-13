87eb522473affce5ace10908265d3364c08f964d
Seo Cahill <seo.cahill@gmail.com>
2013-11-13 15:28:12 +0100

    using heredocs in event decorator

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -48,7 +48,20 @@ class EventDecorator < ApplicationDecorator
   end
 
   def about_section
-    placeholder = auto_html("#Read this first!\n\n---\n\n###You can edit this page by clicking on any element that is marked with an  icon. All your changes should be saved automatically.\n\n####This page uses redcarpet markdown for formating, you can also embed media objects and images.\n\n####Don't forget to mention the lineup for the night:\n1. [Crete Boom](http://creteboom.com/).\n2. [Another Band](http://example.com/).\n\n> fuckin A\n\n####Here's some Crete Boom vids, first youtube:\n\nhttp://www.youtube.com/watch?v=AQ_6sGiglm4\n\nthen vimeo (try resizing the browser all embeds should be responsive)\n\nhttp://vimeo.com/57855999\n\n####Here's a map for the venue\n\nhttps://maps.google.com/maps?q=irish+times&hl=en&ll=37.79483,-122.395914&spn=0.011411,0.019913&sll=37.793694,-122.395828&sspn=0.011412,0.019913&t=h&radius=0.65&hq=irish+times&z=16"){ redcarpet; youtube; vimeo; google_map; link(:target => 'blank') }
+    text = <<-END
+      #Read this first!\n\n---\n\n###You can edit this page by clicking on any element that is marked with an  icon.
+      All your changes should be saved automatically.\n\n
+      ####This page uses redcarpet markdown for formating, you can also embed media objects and images.\n\n
+      ####Don't forget to mention the lineup for the night:\n
+      1. [Crete Boom](http://creteboom.com/).\n2. [Another Band](http://example.com/).\n\n
+      > fuckin A\n\n
+      ####Here's some Crete Boom vids, first youtube:\n\nhttp://www.youtube.com/watch?v=AQ_6sGiglm4\n\n
+      then vimeo (try resizing the browser all embeds should be responsive)\n\nhttp://vimeo.com/57855999\n\n
+      ####Here's a map for the venue\n\n
+      https://maps.google.com/maps?q=irish+times&hl=en&ll=37.79483,-122.395914&spn=0.011411,0.019913&sll=37.793694,
+      -122.395828&sspn=0.011412,0.019913&t=h&radius=0.65&hq=irish+times&z=16
+    END
+    placeholder = auto_html(text){ redcarpet; youtube; vimeo; google_map; link(:target => 'blank') }
     best_in_place_if event_owner?, model, :about, type: :textarea, nil: placeholder, classes: "about", display_as: :about_html, activator: ".edit-about", html_attrs: {"rows" => 12, "cols" => 140, "spellcheck" => "true"}
   end
 
