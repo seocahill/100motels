778a1a05dacc24022d1424a734870fb285b8571f
Seo Cahill <seo.cahill@gmail.com>
2013-11-28 15:13:06 +0100

    pdf gen tests passing

--- test/pdfs/event_pdf_test.rb
+++ test/pdfs/event_pdf_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class EventPdfTest < ActiveSupport::TestCase
   def setup
-    @event = FactoryGirl.create(:event)
+    @event = FactoryGirl.create(:event, name: "Tester")
     @orders = FactoryGirl.create_list(:order, 5, event: @event)
     @tickets = FactoryGirl.create_list(:ticket, 5)
   end
@@ -11,11 +11,26 @@ class EventPdfTest < ActiveSupport::TestCase
     pdf = EventPdf.new(@event, @orders)
     reader = PDF::Reader.new(StringIO.new(pdf.render))
     page = reader.page(1)
-    skip "need to figure out how to test matching contents of pdf"
+    assert page.to_s.include?(@event.name), "should have name"
+    assert page.to_s.include?(@event.date.strftime("%b %d %Y")), "should have date"
+    assert page.to_s.include?(@event.location), "should have date"
+    assert page.to_s =~ /Total Orders: 5/i, "should have order count"
+    assert page.to_s.include?(@orders.first.name), "should have order name"
+    assert page.to_s.include?(@orders.last.email), "should have order email"
+    assert page.to_s.include?(@orders.first.quantity.to_s), "should have order quantity"
+    assert page.to_s.include?(@orders.last.stripe_event.to_s), "should have order statusa"
+    assert page.to_s =~ /50.0/i, "should have order net price"
+    assert page.to_s =~ /52.33/i, "should have order gross price"
   end
 
   test "event ticket pdf generation works" do
     pdf = TicketsPdf.new(@event, @tickets)
-    skip
+    reader = PDF::Reader.new(StringIO.new(pdf.render))
+    page = reader.page(1)
+    assert page.to_s.include?("#{@event.date.strftime("%b %d %Y")} in #{@event.location}"), "header not printing"
+    assert page.to_s.include?(@tickets.first.order.name), "should have order name"
+    assert page.to_s.include?(@tickets.first.order.email), "should have order email"
+    # assert_equal @orders.first.tickets.first.number, "should have ticket number"
+    assert page.to_s.include?(@tickets.first.number), "should have ticket number"
   end
 end
