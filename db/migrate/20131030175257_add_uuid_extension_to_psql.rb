6f4f108b9dd18c6739f1d67d5a58bd92ade9cbb6
Seosamh Cahill <seo.cahill@gmail.com>
2015-05-11 23:52:35 +0200

    same old mistakes

--- db/migrate/20131030175257_add_uuid_extension_to_psql.rb
+++
@@ -1,9 +0,0 @@
-class AddUuidExtensionToPsql < ActiveRecord::Migration
-  def self.up
-    execute "CREATE EXTENSION \"uuid-ossp\";"
-  end
-
-  def self.down
-    execute "DROP EXTENSION \"uuid-ossp\";"
-  end
-end
